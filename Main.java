ef853caf9c114e6df4658211d0c445464e8d0cc4
aryanbalhar123 <aryanbalhar12@gmail.com>
2024-11-19 10:51:24 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,67 +1,58 @@
-class Queue
-{
-    int front, size,capacity;
-    int array[];
+public class Stack {
+    private int arr[];
+    private int size;
+    private int index = 0;
 
-    public Queue (int length) {
-        capacity = length;
-        front = size = 0;
-        array = new int[capacity];
+    public Stack(int size) {
+        this.size = size;
+        arr = new int[size];
     }
-    //QUEUE IS FULL WHEN SIZE BECOMES EQUAL TO THE CAPACITY
-    // THE CAPACITY
-    boolean isFull()
-    {
-        return (size == capacity);
-    }
-    //QUEUE IS EMPTY WHEN SIZE IS 0
-    boolean isEmpty()
-    {
-        return (size == 0);
-    }
-    //method to add an item to the queue.
-    // it change the rear the size
-    void enqueue (int item)
-    {
-         if (isFull() )
-             return;
-         int pos;
-         pos = (front+size)%capacity;
-         array[pos] = item;
-         size=size+1;
-         System.out.println(item+" enqueued to queue");
 
+    public void push(int element) {
+        if (isFull()) {
+            System.out.println("Stack is full");
+            return;
+        }
+        arr[index] = element;
+        index++;
     }
-    //method to remove an item from queue.
-    //it change front and size
-    int dequeue()
-    {
-        if(isEmpty())
-            return 0;
-        int item = array[front];
-        front = (front + 1)%capacity;
-        size = size - 1;
-        return item;
-    }
-    //method to get front of queue
-    int front()
-    {
-        if (isEmpty())
-            return 0;
-    }
-}
 
-public class Lab07 {
-    public static void main(String[]args) {
-        Queue queue = new Queue(10);
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        System.out.println("" +queue.size);
-        queue.enqueue(77);
-
-        System.out.println(queue.dequeue() + "dequeue from queue\n");
-        System.out.println("Front item is " + queue.front());
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1; // or throw an exception
+        }
+        return arr[--index];
     }
-}
+
+    public boolean isEmpty() {
+        return index == 0;
+    }
+
+    public boolean isFull() {
+        return index == size;
+    }
+
+    public int size() {
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(0);
+        stack.push(11);
+        stack.push(13);
+        stack.push(20);
+        stack.push(11);
+
+        System.out.println("1. Size of stack after push operation: " + stack.size());
+
+        System.out.print("2. Pop elements from stack: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+        System.out.println();
+
+        System.out.println("3. Size of stack after pop operation: " + stack.size());
+    }
+}   }
